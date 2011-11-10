9349306d016354d87f6a25d46b8abb78d2cfa32a
Kura <kura@deviling.net>
2011-11-10 21:38:04 +0000

    Comments

--- bozo.py
+++ bozo.py
@@ -4,7 +4,12 @@
 # Original: https://github.com/juuso/BozoCrack
 #
 
+# TODO
+# 
+# * single hash lookup
+
 import sys
+import os
 import md5
 import urllib
 import urllib2
@@ -18,7 +23,7 @@ class Bozo(object):
         self.cracked = {}
 
         for hash in open(file, "r").read().split("\n"):
-            if len(hash) == 32:
+            if len(hash) == 32: # original uses a regex, I figured I'd just check length and be lazy
                 self.hashes.append(hash)
 
         self.hashes = list(set(self.hashes))
@@ -33,12 +38,13 @@ class Bozo(object):
                 print "%s:%s" % (hash, plain_text)
                 self.cache.append(hash)
                 self.cracked[hash] = plain_text
+            # in the original there was a sleep here, I didn't like it. =D
 
     def crack_single(self, hash):
         # Pretend to be Firefox to stop the AWESOME Google 403
         user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
         url = "http://www.google.com/search?q=%s" % hash
-        headers = {'User-Agent': user_agent,} 
+        headers = {'User-Agent': user_agent} 
 
         request = urllib2.Request(url, None, headers)
         response = urllib2.urlopen(request)
@@ -57,7 +63,8 @@ class Bozo(object):
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print "Usage: bozo.py hashes.txt"
-        sys.exit()
+        sys.exit(os.EX_USAGE)
     else:
         bozo = Bozo(sys.argv[1])
         bozo.crack()
+        sys.exit(os.EX_OK)
