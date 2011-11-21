d24aa8cab8a011d24131c30a1b323dbc5ce4cbda
Kura <kura@deviling.net>
2011-11-21 20:43:41 +0000

    Implemented a local file cache to save on Google queries.

--- bozo.py
+++ bozo.py
@@ -13,8 +13,7 @@ class Bozo(object):
 
     def __init__(self, hashes):
         self.hashes = []
-        self.cache = []
-        self.cracked = {}
+        self.cache = {}
 
         # Assume it's a single hash
         if not os.path.exists(hashes):
@@ -24,19 +23,20 @@ class Bozo(object):
                 if len(hash) == 32: # original uses a regex, I figured I'd just check length and be lazy.
                     self.hashes.append(hash)
 
+        self.create_cache()
+        self.load_cache()
         self.hashes = list(set(self.hashes))
-        print "Loaded %s hashes" % len(self.hashes)
+        print "Loaded %s unique hashes" % len(self.hashes)
 
     def crack(self):
         for hash in self.hashes:
-            if hash in self.cache:
-                print "%s:%s" % (hash, self.cracked[hash])
+            if hash in self.cache.keys():
+                print "%s:%s" % (hash, self.cache[hash])
             else:
                 plain_text = self.crack_single(hash)
                 if plain_text:
                     print "%s:%s" % (hash, plain_text)
-                    self.cache.append(hash)
-                    self.cracked[hash] = plain_text
+                    self.write_cache(hash, plain_text)
                 time.sleep(0.1)
 
     def crack_single(self, hash):
@@ -44,7 +44,7 @@ class Bozo(object):
         user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.8 (KHTML, like Gecko) Chrome/17.0.938.0 Safari/535.8"
         # Use the Chrome search URL to pretend to be Chrome.
         url = "http://www.google.co.uk/search?sourceid=chrome&q=%s" % hash
-        headers = {'User-Agent': user_agent} 
+        headers = {'User-Agent': user_agent}
 
         request = urllib2.Request(url, None, headers)
         response = urllib2.urlopen(request)
@@ -58,6 +58,20 @@ class Bozo(object):
         for word in wordlist:
             if hashlib.md5(word).hexdigest() == hash.lower():
                 return word
+
+    def create_cache(self):
+        if not os.path.exists("bozo.cache"):
+            open("bozo.cache", "w").write("")
+
+    def load_cache(self):
+        if os.stat("bozo.cache").st_size != 0:
+            for line in open("bozo.cache", "r").read().strip().split("\n"):
+                hash, plain_text = line.split(":")
+                self.cache[hash] = plain_text
+
+    def write_cache(self, hash, plain_text):
+        with open("bozo.cache", "a") as cache:
+            cache.write("%s:%s\n" % (hash, plain_text))
 
 
 if __name__ == "__main__":
